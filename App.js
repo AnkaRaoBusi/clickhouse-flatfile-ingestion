2dafa7ddd221b05e6af30e254c31e7254a186ff5
AnkaRaoBusi <ankimsdian7@gmail.com>
2025-04-16 18:29:45 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainForm from './components/MainForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>ClickHouse ↔ Flat File Ingestion</h2>
+      <MainForm />
+    </div>
+  );
+}
+export default App;
